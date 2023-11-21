0545da6a3ab6a273b5e880a183713e3cb99e7bee
alihussain <ali.hussaino71205o@gmail.com>
2023-11-21 09:09:35 +0500

    added login page

---
+++ login.js
@@ -0,0 +1,33 @@
+async function signIn() {
+    const Uteamname = document.getElementById('teamName').value;
+    const Upassword = document.getElementById('password').value;
+
+    if (!Uteamname || !Upassword) {
+        document.getElementById('errorbox').innerText = "Please enter the values in.";
+    } else {
+        try {
+            const response = await fetch(`/.netlify/functions/read?teamName=${Uteamname}`);
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            if (data.error) {
+                document.getElementById("status").innerText = `Error: ${data.error}`;
+            } else {
+                const Spassword = data.Team_password;
+
+                if (Spassword === Upassword) {
+                    // Redirect to index.js or another page on the server side
+                    window.location.href = '/index.js'; // Adjust the URL as needed
+                } else {
+                    document.getElementById('errorbox').innerText = "Password Wrong";
+                }
+            }
+        } catch (error) {
+            document.getElementById('errorbox').innerText = "An error occurred during sign-in.";
+        }
+    }
+}
