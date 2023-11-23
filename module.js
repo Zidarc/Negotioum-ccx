083c619ceced1a0faaa8a10c23d617b2b71fbdee
Ali Hussain <alihussain20057@gmail.com>
2023-11-23 22:58:17 +0500

    made some changes, called the function when running DOM

--- module.js
+++ module.js
@@ -1,8 +1,7 @@
 import { getUserData } from './teamdata.js';
 
 // Example usage
-const userData = getUserData();
-console.log(userData.UteamName, userData.Upassword);
+
 document.addEventListener("DOMContentLoaded", async function () { 
     try {
         const teamName = 'MasterCoins';
@@ -29,8 +28,10 @@ document.addEventListener("DOMContentLoaded", async function () {
         document.getElementById("status").innerText = "Error: " + error;
     }
 });
-document.getElementById("readButton").addEventListener("click", async function () {
+document.addEventListener("DOMContentLoaded", async function () {
     try {
+        const userData = getUserData();
+        console.log(userData.UteamName, userData.Upassword);
         const response = await fetch(`/.netlify/functions/read?teamName=${userData.UteamName}`);
 
         if (!response.ok) {
