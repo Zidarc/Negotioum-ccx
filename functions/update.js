22fef24ad73fc2127d0c4522998acc6d04f36411
alihussain <ali.hussaino71205o@gmail.com>
2023-12-05 22:03:51 +0500

    errrr

--- functions/update.js
+++ functions/update.js
@@ -101,7 +101,7 @@ exports.handler = async (event, context) => {
         } else if (type === 2) {
             if (coinVal <= userCoinVal) {
                 const updatedData = await UserData.findOneAndUpdate(
-                    { Team_name: teamName },
+                    { Team_name: teamId },
                     {
                         $set: { [`coins.${index}`]: (userCoinVal - parseFloat(coinVal)) },
                         $inc: { free_money: parseFloat(coinVal) * serverCoinVal },
