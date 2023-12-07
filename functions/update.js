1a921228f93eebc937c689f64799810390e40039
alihussain <ali.hussaino71205o@gmail.com>
2023-12-07 23:27:34 +0500

    err

--- functions/update.js
+++ functions/update.js
@@ -66,10 +66,11 @@ exports.handler = async (event, context) => {
         const serverCoinVal = masterCoin[index];
 
         // Calculate total function
+        let sum = 0;
         async function calculateTotal() {
             // Calculate the sum of products of corresponding elements
             if (masterCoin.length === userCoins.length) {
-                let sum = masterCoin.reduce((acc, masterCoinVal, idx) => acc + masterCoinVal * userCoins[idx], 0);
+                sum = masterCoin.reduce((acc, masterCoinVal, idx) => acc + masterCoinVal * userCoins[idx], 0);
                 // Calculate total by adding sum and freeCoins
                 const total = sum + freeCoins;
                 return total;
