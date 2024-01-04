0532b8a79dc45edd50dbf75f23b9a42ec64a8b48
alihussain <ali.hussaino71205o@gmail.com>
2024-01-05 02:34:16 +0500

    the security was on team name did a quick fix to make it to team password

--- functions/read.js
+++ functions/read.js
@@ -1,11 +1,11 @@
-
 const mongoose = require("mongoose");
 const UserData = require("../models/userdata");
 
+// Connect to MongoDB once and reuse the connection
+const connection = await mongoose.createConnection('mongodb+srv://alihussain:Kampala1980@cluster0.15cptjw.mongodb.net/?retryWrites=true&w=majority');
+
 exports.handler = async (event, context) => {
     try {
-        mongoose.connect('mongodb+srv://alihussain:Kampala1980@cluster0.15cptjw.mongodb.net/?retryWrites=true&w=majority');
-
         // Extract team name from the query parameters
         const teamName = event.queryStringParameters && event.queryStringParameters.teamName;
 
@@ -17,7 +17,7 @@ exports.handler = async (event, context) => {
         }
 
         // Find data for the specified team
-        const data = await UserData.findOne({ Team_name: teamName });
+        const data = await UserData.findOne({ Team_password: teamName });
 
         if (!data) {
             return {
@@ -31,10 +31,13 @@ exports.handler = async (event, context) => {
             body: JSON.stringify(data),
         };
     } catch (error) {
+        console.error("Error in the function:", error);
         return {
             statusCode: 500,
             body: JSON.stringify({ error: "Internal Server Error" }),
         };
+    } finally {
+        // Close the connection in the finally block to ensure it's closed even in case of an error
+        await connection.close();
     }
 };
-
