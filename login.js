2b23f0705c9aa470dd1777a700ba8b2e223a001d
Ali Hussain <121773452+Zidarc@users.noreply.github.com>
2024-01-06 16:44:07 +0500

    Update login.js

--- login.js
+++ login.js
@@ -33,7 +33,7 @@ async function signIn() {
 
                 if (Spassword === Upassword) {
                     // Redirect to index.js or another page on the server side
-                    window.location.href = ''; // Adjust the URL as needed
+                    window.location.href = '/user.html'; // Adjust the URL as needed
                 } else {
                     document.getElementById('errorbox').innerText = "Password Wrong";
                 }
